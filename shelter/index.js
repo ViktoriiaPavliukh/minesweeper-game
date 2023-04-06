632c07dd6358d6a650b97e7751b85dd045a9896e
Viktoriia Pavliukh <victoriataran.mail@gmail.com>
2023-04-06 23:11:24 +0100

    feat: add burger-menu Thu, 06 April 2023 23:11

--- shelter/index.js
+++ shelter/index.js
@@ -1,1 +1,90 @@
-console.log("Oценка - 100 баллов \n Страница Main (60) \n Страница Pets (40)") 
+const burgerBtn = document.querySelector('#burgerBtn');
+
+const btnLeft = document.querySelector('#btnLeft');
+const btnRight = document.querySelector('#btnRight');
+const carousel = document.querySelector('#carousel');
+const images = document.querySelectorAll('.cards__image');
+const slider = document.querySelector('#slider');
+
+burgerBtn.addEventListener('click', function() {
+    document.querySelector('.background').classList.toggle('open')
+    
+})
+
+
+let offset = 0;
+
+btnRight.addEventListener('click', function(){
+    offset = offset + slider.offsetWidth;
+
+    if(offset > 990) {
+        offset = 0;
+    }
+    carousel.style.left = -offset + 'px';
+})
+
+btnLeft.addEventListener('click', function(){
+    offset = offset - slider.offsetWidth;
+    carousel.style.left = -offset + 'px';
+})
+// let count = 0;
+// let width;
+
+// function init() {
+//     console.log('resize');
+//     width = slider.offsetWidth;
+//     carousel.getElementsByClassName.width = width*images.length + 'px';
+//     images.forEach( item => {
+//         item.style.width = width + 'px'
+//         item.style.height = 'auto';
+//     })
+//     rollSlider();
+// }
+// window.addEventListener('resize', init);
+// init();
+
+
+// btnRight.addEventListener('click', function() {
+//     count++;
+//     if (count < images.length) {
+//         count = 0;
+//     }
+//     rollSlider();
+// });
+
+// function rollSlider() {
+//     carousel.style.transform = 'translate(- '+ count * width+'px)';
+// }
+
+
+
+// const moveLeft = () => {
+//     carousel.classList.add('transition-left');
+//     btnLeft.removeEventListener('click', moveLeft);
+//     btnRight.removeEventListener('click', moveRight);
+// }
+
+// const moveRight = () => {
+//     carousel.classList.add('transition-right');
+//     btnLeft.removeEventListener('click', moveLeft);
+//     btnRight.removeEventListener('click', moveRight);
+// }
+
+// carousel.addEventListener('animationend', (animationEvent) => {
+//     if (animationEvent.animationName === 'move-left') {
+//         carousel.classList.remove('transition-left');
+//         const leftItems = document.querySelector('#items-left').innerHTML;
+//         document.querySelector('#active-items').innerHTML = leftItems
+//     } else {
+//         carousel.classList.remove('transition-right');
+//     }
+    
+//     btnLeft.addEventListener('click', moveLeft);
+//     btnRight.addEventListener('click', moveRight);
+// })
+// console.log()
+
+
+// btnLeft.addEventListener('click', moveLeft);
+// btnRight.addEventListener('click', moveRight);
+
---
+++ shelter/pages/pets/index.js
@@ -0,0 +1,5 @@
+const btnLight = document.querySelector('#btnLight');
+
+btnLight.addEventListener('click', function() {
+    document.querySelector('.header').classList.toggle('open')
+})
