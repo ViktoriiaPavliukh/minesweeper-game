d2affdf5981fcdb5217ca1c7a70387047cea4572
Viktoriia Pavliukh <victoriataran.mail@gmail.com>
2023-05-16 22:20:54 +0100

    feat: add logicof mines finding

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,36 +1,36 @@
-const boardSize = 10;
-const numMines = 10;
-let gameBoard = [];
 
-function createBoard() {
-  for (let i = 0; i < boardSize; i++) {
-    let row = [];
-    for (let j = 0; j < boardSize; j++) {
-      row.push({
-        isMine: false,
-        revealed: false,
-        value: 0
-      });
-    }
-    gameBoard.push(row);
-  }
-}
-
-function renderBoard() {
+function renderBoard(width, height, mines) {
   const container = document.createElement('div');
   const minesweeperWrapper = document.createElement('div');
+  const cellsAmount = width * height;
   container.className = "container";
   minesweeperWrapper.className = "minesweeper";
   document.body.appendChild(container);
   container.appendChild(minesweeperWrapper);
+  minesweeperWrapper.innerHTML = `<button></button>`.repeat(cellsAmount);
+  const cells = [...minesweeperWrapper.children];
+  
+ 
+  const bombs = [...Array(cellsAmount).keys()]
+  .sort(() => Math.random() - 0.5)
+  .slice(0, mines);
 
-  for (let row = 0; row < boardSize; row++) {
-    for (let col = 0; col < boardSize; col++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      minesweeperWrapper.appendChild(cell);
+  minesweeperWrapper.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'BUTTON') {
+      return console.log('not field');
     }
-  }
-}
 
-renderBoard();
+    const index = cells.indexOf(event.target);
+    const col = index % width;
+    const row = Math.floor(index / width);
+    event.target.innerHTML = isBomb(row, col) ? 'X' : ' ';
+    event.target.disabled = true;
+  });
+
+  function isBomb(row, col) {
+    const index =  row * width + col; 
+    return bombs.includes(index);
+  }
+} 
+
+renderBoard(10, 10, 10)
