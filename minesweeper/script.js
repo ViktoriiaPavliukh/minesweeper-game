bcacbdbae2a1b008335bbe9f9d0c7c541da8de4f
Viktoriia Pavliukh <victoriataran.mail@gmail.com>
2023-05-18 09:46:39 +0100

    feat: add timer

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,16 +1,34 @@
 
 function renderBoard(width, height, mines) {
   const container = document.createElement('div');
+  const header =  document.createElement('header');
   const minesweeperWrapper = document.createElement('div');
   const cellsAmount = width * height;
+  let divTimer = document.createElement('div');
+  let startTime;
+  let moves = 0;
+  let timer = 0;
+
   container.className = "container";
+  header.className = "header";
   minesweeperWrapper.className = "minesweeper";
+  divTimer.className = "timer";
+  divTimer.innerText = "00:00";
   document.body.appendChild(container);
   container.appendChild(minesweeperWrapper);
-  minesweeperWrapper.innerHTML = `<button></button>`.repeat(cellsAmount);
-  const cells = [...minesweeperWrapper.children];
+  container.appendChild(header);
+  header.appendChild(divTimer);
+
+  for (let i = 0; i < cellsAmount; i++) {
+    const button = document.createElement('button');
+    button.className = 'cell';
+    minesweeperWrapper.appendChild(button);
+  }
   
- 
+  const cells = [...minesweeperWrapper.children];
+  let closedCellsCount = cellsAmount; 
+  let isFirstMove = true;
+
   const bombs = [...Array(cellsAmount).keys()]
   .sort(() => Math.random() - 0.5)
   .slice(0, mines);
@@ -23,11 +41,54 @@ function renderBoard(width, height, mines) {
     const index = cells.indexOf(event.target);
     const col = index % width;
     const row = Math.floor(index / width);
+    
+    if (isFirstMove) {
+      isFirstMove = false;
+      startTime = performance.now();
+      placeMines(row, col);
+    }
+
     openCell(row, col);
   });
 
+  function timerFunc() {
+    let seconds = 1;
+    let minutes = 0;
+    timer = setInterval(() => {
+        if (seconds >= 60) {
+            minutes++;
+            seconds = 0;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes >= 10) {
+            divTimer.innerText = `${minutes}:${seconds}`;
+        } else {
+            divTimer.innerText = `0${minutes}:${seconds}`;
+        }
+        seconds++;
+    }, 1000);
+}
+timerFunc();
+
   function isValid(row, col) {
     return row >= 0 && row < height && col >= 0 && col < width;
+  }
+
+  function placeMines(initialRow, initialCol) {
+    const initialIndex = initialRow * width + initialCol;
+    const excludedIndexes = [initialIndex];
+
+    bombs.length = 0 ;
+    for (let i = 0; i < mines; i++) {
+      let randomIndex;
+      do {
+        randomIndex = Math.floor(Math.random() * cellsAmount);
+      } while (excludedIndexes.includes(randomIndex));
+        bombs.push(randomIndex);
+        excludedIndexes.push(randomIndex);
+    }
   }
 
   function getMinesCount(row, col) {
@@ -48,10 +109,29 @@ function renderBoard(width, height, mines) {
     const cell = cells[index];
 
     if (cell.disabled === true) return;
+
     cell.disabled = true;
+
     if (isBomb(row, col)) {
-      cell.innerHTML = 'X';
-      alert('Looser');
+      cell.innerHTML = '💣';
+      cell.classList.add('red');
+      alert('Game over. Try again');
+      clearInterval(timer);
+      return;
+    }
+
+    closedCellsCount--;
+    // if (!cell.classList.contains('opened')) {
+    //   cell.classList.add('opened');
+    //   moves++;
+    //   console.log(moves);
+    // } 
+
+    if (closedCellsCount <= mines) {
+      const endTime = performance.now();
+      const elapsedTime = Math.floor((endTime - startTime) / 1000);
+      alert (`Hooray! You found all mines in ${elapsedTime} seconds and N moves!!`);
+      clearInterval(timer);
       return;
     }
 
@@ -66,9 +146,7 @@ function renderBoard(width, height, mines) {
        openCell(row + y, col + x);
          
        }
-      }
-    // cell.innerHTML = isBomb(row, col) ? 'X' : getMinesCount(row, col);
-    
+      }  
   }
 
   function isBomb(row, col) {
@@ -78,4 +156,4 @@ function renderBoard(width, height, mines) {
   }
 }
 
-renderBoard(10, 10, 10)
+renderBoard(10, 10, 10);
